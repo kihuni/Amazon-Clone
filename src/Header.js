26bfb5a977376b7bf97f8a2c54dd7c48408c2bfe
Stephen kihuni <stephenkihuni55@gmail.com>
2021-12-22 18:14:37 +0300

    amazon-clone header

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="header_logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+
+      <div className="header_search">
+        <input className="header_searchInput" type="text" />
+        <SearchIcon className="header_searchIcon"/>    
+      </div>
+      <div className="header_nav">
+          <div className="header_option">
+            <span className="header_optionLineOne">Hello Guest</span>
+            <span className="header_optionLineTwo">Sign in</span>
+          </div>
+          <div className="header_option">
+          <span className="header_optionLineOne">Returns</span>
+            <span className="header_optionLineTwo">& Order</span>
+              </div>
+              <div className="header_option">
+              <span className="header_optionLineOne">Your</span>
+            <span className="header_optionLineTwo">Prime</span>
+              </div>
+              <div className="header_optionBasket">
+                <ShoppingBasketIcon />
+                <span className="header_optionLineOne">0</span>
+              </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
